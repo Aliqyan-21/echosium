fdf557e10cf0e6e0df043f18b51dbfe8e0d70d64
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-12-25 19:09:49 +0530

    fix: some json structure fixes in field name

--- jamendo/jamendo.go
+++ jamendo/jamendo.go
@@ -13,15 +13,15 @@ import (
 type Track struct {
 	ID            string `json:"id"`
 	Name          string `json:"name"`
-	TrackUrl      string `json:"song"`
-	Artist        string `json:"artist"`
-	Album         string `json:"album"`
+	TrackUrl      string `json:"audiodownload"`
+	Artist        string `json:"artist_name"`
+	Album         string `json:"album_name"`
 	Image         string `json:"image"`
 	AudioFormat   string `json:"audioformat"`
-	AudioDownload bool   `json:"audiodownload"`
+	AudioDownload bool   `json:"audiodownload_allowed"`
 }
 
-func getTracks(clientID, mood string) ([]Track, error) {
+func GetTracks(clientID, mood string) ([]Track, error) {
 	url := fmt.Sprintf("https://api.jamendo.com/v3.0/tracks/?client_id=%s&fuzzytags=%s&format=jsonpretty", clientID, mood)
 
 	resp, err := http.Get(url)
