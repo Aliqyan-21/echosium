d40fa869b9580bc54d07e61f3418706369990fdf
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-12-25 18:53:50 +0530

    feat: jamendo fetching tracks according to mood param

--- jamendo/jamendo.go
+++ jamendo/jamendo.go
@@ -1,5 +1,15 @@
+// package jamendo is the package that integrates the jamendo api in echosium
 package jamendo
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Track struct represents a Jamendo track
 type Track struct {
 	ID            string `json:"id"`
 	Name          string `json:"name"`
@@ -11,6 +21,32 @@ type Track struct {
 	AudioDownload bool   `json:"audiodownload"`
 }
 
-func getTracks() {
+func getTracks(clientID, mood string) ([]Track, error) {
+	url := fmt.Sprintf("https://api.jamendo.com/v3.0/tracks/?client_id=%s&fuzzytags=%s&format=jsonpretty", clientID, mood)
 
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("API error: %s", string(body))
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+	// fmt.Printf("raw: %s", string(body))
+
+	resp.Body = io.NopCloser(bytes.NewReader(body))
+
+	var result struct {
+		Results []Track `json:"results"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+	return result.Results, nil
 }
