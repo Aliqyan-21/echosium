9b8df7388d744c6d41250d689d5c5f8537f9f0d9
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-12-25 19:10:17 +0530

    feat: start command to play music with -mood param to insert mood

--- cmd/start.go
+++ cmd/start.go
@@ -1,10 +1,17 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
 
+	"github.com/aliqyan-21/echosium/jamendo"
 	"github.com/spf13/cobra"
 )
+
+var mood string
 
 // startCmd represents the start command
 var startCmd = &cobra.Command{
@@ -12,20 +19,62 @@ var startCmd = &cobra.Command{
 	Short: "plays the music based on the mood, default mood is focus",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("music is playing")
+		confFile, err := os.Open("config.json")
+		if err != nil {
+			fmt.Printf("Failed to open config file: %v\n", err)
+			return
+		}
+		defer confFile.Close()
+
+		var config map[string]string
+		if err := json.NewDecoder(confFile).Decode(&config); err != nil {
+			fmt.Printf("Failed to parse config: %v\n", err)
+			return
+		}
+
+		clientID, exists := config["client_id"]
+		if !exists || clientID == "" {
+			fmt.Println("Client ID not found in config.json.")
+			return
+		}
+
+		fmt.Printf("Fetching tracks for mood: %s\n", mood)
+		tracks, err := jamendo.GetTracks(clientID, mood)
+		if err != nil {
+			fmt.Printf("Failed to fetch tracks: %v\n", err)
+			return
+		}
+
+		if len(tracks) == 0 {
+			fmt.Printf("No tracks found for mood : %s.", mood)
+		} else {
+			randIdx := rand.Intn(len(tracks))
+			track := tracks[randIdx]
+
+			fmt.Printf("Now playing: %s by %s\n", track.Name, track.Artist)
+
+			playMusic(track.TrackUrl)
+		}
 	},
 }
 
+func playMusic(trackUrl string) {
+	_, err := exec.LookPath("mpv")
+	if err != nil {
+		fmt.Println("Error: mpv is not installed or not in your PATH.")
+		return
+	}
+
+	cmd := exec.Command("mpv", "--no-audio-display", trackUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error playing music: %v\n", err)
+	}
+}
+
 func init() {
+	startCmd.Flags().StringVarP(&mood, "mood", "m", "relaxation", "Specify the mood for the music tracks (e.g., peaceful, upbeat, relaxed)")
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
